2ce06747c27993b7b75f4b86d996880c65e44317
CasaOne69 <oficina@casalock.es>
2023-07-27 15:44:29 +0200

    Finished

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const conBut = document.getElementById("conbut")
 const userI = document.getElementById("useri")
 const return1 = document.getElementById("leng")
 const return2 = document.getElementById("vols")
+const return3 = document.getElementById("massr")
 
 conBut.addEventListener("click", function() {
 lengReturn ()
 volReturn ()
+massReturn ()
 userI.value = ""
 })
 
@@ -41,6 +43,22 @@ function volReturn () {
     ${userI.value} gallons = ${Number(userI.value / 0.264).toFixed(3) } liters
     `
     return2.textContent = volReturn
+    }
+    
+
+}
+
+function massReturn () {
+    if (userI.value < 2) {
+        let masReturn = `${(userI.value)} kilo = ${Number(userI.value * 2.204).toFixed(3) } pounds | 
+        ${userI.value} pound = ${Number(userI.value / 2.204).toFixed(3) } kilos
+        `
+        return3.textContent = masReturn  
+    } else {
+        let masReturn = `${(userI.value)} kilos = ${Number(userI.value * 2.204).toFixed(3) } pounds | 
+    ${userI.value} pounds = ${Number(userI.value / 2.204).toFixed(3) } kilos
+    `
+    return3.textContent = masReturn
     }
     
 
